528a17f496229d0cccb55944f7e2ab3dc15eec36
Maary <24504742+Steve-Mr@users.noreply.github.com>
2024-05-12 22:17:49 +0800

    mainactivity edgetoedge & bettter statusbar look

--- app/src/main/java/com/maary/yetanothercalendarwidget/ui/theme/Theme.kt
+++ app/src/main/java/com/maary/yetanothercalendarwidget/ui/theme/Theme.kt
@@ -45,7 +45,7 @@ fun YetAnotherCalendarWidgetTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
@@ -57,8 +57,8 @@ fun YetAnotherCalendarWidgetTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+//            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
